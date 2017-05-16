f1dacac3dbad3220c33b4fdffed2d75d695e2061
wgadd831 <wgadd831@PR09.hhs.win.fuhsd.org>
2017-05-16 13:54:18 -0700

    started working on safeSquare

--- SuperRectangleAdventure/src/SafeSquare.java
+++ SuperRectangleAdventure/src/SafeSquare.java
@@ -1,18 +1,30 @@
+import java.awt.Shape;
+import java.util.ArrayList;
 
 public class SafeSquare {
 	
-	
-	
+	private ArrayList<Shape> obstacles;
+	private boolean isStart;
 	
 	
 	/**
 	 *  Creates an instance of the SafeSquare object 
-	 *  @param x determines what side of the screen the square is made on (true --> left, false --> right) 
+	 *  @param x determines what side of the screen the square is made on (true --> left, false --> right)
+	 *  @param x determines if the safe square is the starting point or the ending point 
 	 *  
 	*/
-	public SafeSquare(boolean x)
+	public SafeSquare(boolean x, boolean isStart)
 	{
+		this.isStart = isStart;
 		
+		if(x)
+		{
+			
+		}
+		else
+		{
+			
+		}
 	}
 
 }
